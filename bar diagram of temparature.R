2c7b87f1937d0c21321f1475fda75122394ab61e
PRIYASHA SINGHA <95346970+Priyasha21-SNU@users.noreply.github.com>
2022-07-10 14:00:20 +0530

    Add files via upload

---
+++ bar diagram of temparature.R
@@ -0,0 +1,4 @@
+temparature_data<-c(27,28,29,26,35,37,39)
+days<-c("sun","mon","tue","thr","wed","fri","sat")
+
+barplot(temparature_data,xlab="days",ylab="temparature",main="temparature variation",names.arg="days",col="sky blue",border="Red")
